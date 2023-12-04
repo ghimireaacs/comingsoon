3c9a79a91c238d55b0efdc140478a4aaaf2998c1
ghimireaacs <aacs.csu@gmail.com>
2023-12-05 04:41:26 +1100

    modified socials

--- src/components/body/socials.js
+++ src/components/body/socials.js
@@ -4,15 +4,15 @@ export default function Socials() {
   return (
     <div class="grid grid-cols-2 md:grid-cols-2 gap-4">
       <a
-        href="https://www.facebook.com/"
-        title="Quicktoolz On Facebook"
+        href="https://www.youtube.com/@confession_nepal"
+        title="Confession Nepal On YouTube"
         class="md:w-32 tracking-wide font-bold rounded border-2 border-indigo-500 hover:text-white hover:border-red-600 hover:bg-red-600 shadow-md py-2 px-6 inline-flex items-center transition duration-500"
       >
         <span class="mx-auto">Youtube</span>
       </a>
       <a
-        href="https://twitter.com"
-        title="Quicktoolz On Twitter"
+        href="https://www.tiktok.com/@confession_nepal"
+        title="Confession Nepal On TikTok"
         class="md:w-32 tracking-wide font-bold rounded border-2 border-indigo-500 hover:text-white hover:border-blue-500 hover:bg-blue-500 shadow-md py-2 px-6 inline-flex items-center transition duration-500"
       >
         <span class="mx-auto">TikTok</span>
